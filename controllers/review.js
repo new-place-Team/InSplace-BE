82e25a42df956fa5903a9ef048f5c8828ee10039
fnrkp089 <fnrkp089@gmail.com>
2021-10-26 04:03:53 +0900

    리뷰작성 api, s3업로드 js

--- controllers/review.js
+++ controllers/review.js
@@ -1,6 +1,33 @@
-var express = require('express');
-var router = express.Router();
 const logger = require('../config/logger');
 const {db} = require('../models/index');
 
-module.exports = router;
+/* 리뷰 등록 함수 */ 
+
+const postingReview = async(req, res) => {
+  const postId = req.params.postId;
+  const review_images = req.file.transforms[0].location;
+  const {review_desc, revisit_yn} = req.body
+  try{
+    const ReviewQuery = `
+    INSERT INTO 
+    Reviews(post_id, review_images, review_desc, revisit_yn, review_delete_yn)
+    VALUES(?, ?, ?, ?, ?)
+    `;
+    await db.query(ReviewQuery, [postId, review_images, review_desc, revisit_yn, 0], (err, rows, fields) => {});
+    return res.status(201).json({
+      success: true,
+    });
+  } catch(err) {
+    logger.error('리뷰 등록에서 예상치 못한 에러가 발생했습니다:', err);
+    return res.status(400).json({
+      success: false,
+      errMsg: `리뷰 등록에서 예상치 못한 에러가 발생했습니다: ${err}`,
+    });
+  } finally {
+    db.release(); // db 회수
+  }
+}
+
+module.exports = {
+  postingReview,
+}
