339ae59b12d24df30ff7a2acd97ae7079ed5336c
KimGiTae <rlxo6919@naver.com>
2021-10-27 00:59:44 +0900

    로컬 로그인 기능

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,10 @@
 const logger = require('../config/logger');
 const { pool } = require('../models/index');
 const bcrypt = require('bcrypt');
-
-const userRegist = async (req, res) => {
+const jwt = require('jsonwebtoken');
+const registUser = async (req, res) => {
   const { email, nickname, password } = req.user;
   const { male_yn, mbti_id } = req.body;
-
   //Email 중복 검사 함수 선언
   const duplicateEmailCheck = async (email) => {
     const [result] = await pool.query(`SELECT * FROM Users WHERE email = ?`, [
@@ -33,7 +32,6 @@ const userRegist = async (req, res) => {
       .status(200)
       .json({ success: true, msg: '닉네임이 중복되었습니다' });
   }
-
   //중복검사 통과
   try {
     //mbti id검사
@@ -77,4 +75,47 @@ const userRegist = async (req, res) => {
   }
 };
 
-module.exports = userRegist;
+const authUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    //유저가 입력한 이메일로 해쉬된 비밀번호 가져오기
+    const [userPasswordAndId] = await pool.query(
+      `SELECT password, user_id, nickname 
+       FROM Users 
+       WHERE email = ?`,
+      [email]
+    );
+    //해쉬된 비밀번호가 없는경우는 이메일이 없는경우이므로 로그인 실패
+    if (userPasswordAndId.length == 0) {
+      return res
+        .status(200)
+        .json({ success: true, msg: '이메일 혹은 패스워드가 틀렸습니다.' });
+    }
+    const { user_id, nickname } = userPasswordAndId[0];
+    // 해쉬된 비밀번호와 유저가 입력한 비밀번호를 비교
+    const comparePassword = await bcrypt.compare(
+      password,
+      userPasswordAndId[0].password
+    );
+    //true면 로그인 성공
+    if (comparePassword == true) {
+      //jsontoken만들기
+      const token = jwt.sign({ user_id }, process.env.SECRET_KEY, {
+        expiresIn: '60m',
+      });
+      return res.status(200).json({ success: true, token, nickname });
+    }
+    //비밀번호가 틀려서 로그인 실패
+    return res
+      .status(200)
+      .json({ success: true, msg: '이메일 혹은 패스워드가 틀렸습니다' });
+  } catch (err) {
+    logger.error(`해쉬된 비밀번호 가지고오는 과정에서 에러 : ${err}`);
+    return res.status(400).json({
+      success: false,
+      errMSG: '해쉬된 비밀번호 가지고 오는 과정에서 에러',
+      err,
+    });
+  }
+};
+module.exports = { registUser, authUser };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const valudationUser = require('../middlewares/valudationUser');
-const userRegist = require('../controllers/user');
+const { registUser, authUser } = require('../controllers/user');
 /* GET users listing. */
-router.post('/register', valudationUser, userRegist);
+router.post('/register', valudationUser, registUser);
+router.post('/auth', authUser);
 
 module.exports = router;
