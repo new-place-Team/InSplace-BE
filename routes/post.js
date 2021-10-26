d0a70e86db8281cadebae278e47e1dfaafc3b24b
jongwanra <najongsi@gmail.com>
2021-10-26 18:14:51 +0900

    [211026]  DB 설계 수정(전체적으로)

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const reviewRouter = require('./review');
 const likeRouter = require('./like');
+const { addVisitedList } = require('../controllers/post');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
@@ -9,5 +10,8 @@ router.get('/', function (req, res, next) {
 
 router.use('/:postId/reviews', reviewRouter);
 router.use('/:postId/likes', likeRouter);
+
+/* 가본 장소 리스트에 추가 */
+router.post('/:postId/visited', addVisitedList);
 
 module.exports = router;
