764573e6d48d9ea039c589b409fede46eb006ab5
KimGiTae <rlxo6919@naver.com>
2021-10-27 16:19:28 +0900

    Auth promise로 변경

--- controllers/post.js
+++ controllers/post.js
@@ -3,14 +3,15 @@ const { pool } = require('../models/index');
 /* 가본 장소 리스트에 추가 */
 const addVisitedList = (req, res) => {
   try {
-    const query = `
-    INSERT INTO VisitedPosts()
-    `;
+    console.log(req.params);
+
+    // await pool.query('INSERT INTO VisitedPosts');
   } catch (err) {
-    logger.error(`가본 장소 리스트 추가 API에서 발생한 오류: ${err}`);
-    return res.status(400).json({
+    logger.error(`가본 장소 리스트 추가부분에서 에러:${err}`);
+    res.status(400).json({
       success: false,
-      errMsg: `가본 장소 리스트 추가 API에서 발생한 오류: ${err}`,
+      errMsg: '가본 장소 리스트 추가부분에서 에러',
+      err: err,
     });
   }
 };
@@ -18,16 +19,25 @@ const addVisitedList = (req, res) => {
 const showDetailPost = async (req, res) => {
   const { postId } = req.params;
   //상세페이지 찾는 쿼리
-  const [result] = await pool.query(
-    `SELECT title, post_images, post_loc_x, post_loc_y, description, address, address_short, post_desc, like_cnt FROM Posts 
+  try {
+    const [result] = await pool.query(
+      `SELECT title, post_images, post_loc_x, post_loc_y, description, address, address_short, post_desc, like_cnt FROM Posts 
      INNER JOIN Categories 
      ON Posts.category_id = Categories.category_id  
      WHERE post_id = ? `,
-    [postId]
-  );
-  //payload
-  const payload = { success: true, ...result[0] };
-  res.status(200).json({ payload });
+      [postId]
+    );
+    //payload
+    const payload = { success: true, ...result[0] };
+    res.status(200).json({ payload });
+  } catch (err) {
+    logger.error(`상세페이지 찾는쿼리에서 에러 : ${err}`);
+    res.status(400).json({
+      success: false,
+      errMsg: '상세페이지 찾는 쿼리에서 에러',
+      err: err,
+    });
+  }
 };
 module.exports = {
   addVisitedList,
--- routes/post.js
+++ routes/post.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const reviewRouter = require('./review');
 const likeRouter = require('./like');
-const postDetail = require('./postDetail');
+const showDetailRouter = require('./detail');
+const addVisited = require('./visited');
 const { addVisitedList } = require('../controllers/post');
 
 router.use('/:postId/reviews', reviewRouter);
 router.use('/:postId/likes', likeRouter);
-router.use('/:postId', postDetail);
-router.use('/:postId/visited', likeRouter);
+router.use('/:postId', showDetailRouter);
+router.use('/:postId/visited', addVisited);
 
 /* 가본 장소 리스트에 추가 */
 router.post('/:postId/visited', addVisitedList);
