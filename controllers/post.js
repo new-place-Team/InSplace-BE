db0023cee85537fa52a95aa69cb462aa57f127ea
KimGiTae <rlxo6919@naver.com>
2021-10-27 15:11:39 +0900

    payload로 변경

--- controllers/post.js
+++ controllers/post.js
@@ -17,6 +17,7 @@ const addVisitedList = (req, res) => {
 
 const showDetailPost = async (req, res) => {
   const { postId } = req.params;
+  //상세페이지 찾는 쿼리
   const [result] = await pool.query(
     `SELECT title, post_images, post_loc_x, post_loc_y, description, address, address_short, post_desc, like_cnt FROM Posts 
      INNER JOIN Categories 
@@ -24,8 +25,9 @@ const showDetailPost = async (req, res) => {
      WHERE post_id = ? `,
     [postId]
   );
-  const payload = { ...result[0] };
-  res.status(200).json({ success: true, payload });
+  //payload
+  const payload = { success: true, ...result[0] };
+  res.status(200).json({ payload });
 };
 module.exports = {
   addVisitedList,
--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,6 @@ const registUser = async (req, res) => {
   const { email, nickname, password } = req.user;
   const { male_yn, mbti } = req.body;
   //Email 중복 검사 함수 선언
-
   const checkDuplicateOfEmail = async (email) => {
     try {
       const [result] = await pool.query(`SELECT * FROM Users WHERE email = ?`, [
@@ -37,17 +36,23 @@ const registUser = async (req, res) => {
         .json({ success: false, errMSG: 'Nickname 중복검사 에러', err: err });
     }
   };
+  //기본 payload
+  let payload = {
+    success: true,
+  };
+
   //Email 중복검사
   if (await checkDuplicateOfEmail(email)) {
-    return res
-      .status(200)
-      .json({ success: true, msg: '이메일이 중복되었습니다' });
+    //함수를 불러와서 결과를 payload에 담아주기
+    payload.msg = '이메일이 이미 존재합니다';
+
+    return res.status(200).json({ payload });
   }
   //Nickname 중복검사
   if (await checkDuplicateOfNickname(nickname)) {
-    return res
-      .status(200)
-      .json({ success: true, msg: '닉네임이 중복되었습니다' });
+    //함수를 불러와서 결과를 payload에 담아주기
+    payload.msg = '닉네임이 이미 존재합니다';
+    return res.status(200).json({ payload });
   }
   //중복검사 통과
   try {
@@ -73,7 +78,7 @@ const registUser = async (req, res) => {
         [email, nickname, hashPassword, male_yn, data[0].mbti_id]
       )
       .then((data) => {
-        return res.status(201).json({ success: true });
+        return res.status(201).json({ payload });
       })
       .catch((err) => {
         logger.error(`유저 회원가입부분에서 에러 발생 : ${err}`);
@@ -95,6 +100,10 @@ const registUser = async (req, res) => {
 
 const authUser = async (req, res) => {
   const { email, password } = req.body;
+  //기본 payload
+  let payload = {
+    success: true,
+  };
   try {
     //유저가 입력한 이메일로 해쉬된 비밀번호 가져오기
     const [userPasswordAndId] = await pool.query(
@@ -105,9 +114,9 @@ const authUser = async (req, res) => {
     );
     //해쉬된 비밀번호가 없는경우는 이메일이 없는경우이므로 로그인 실패
     if (userPasswordAndId.length == 0) {
-      return res
-        .status(200)
-        .json({ success: true, msg: '이메일 혹은 패스워드가 틀렸습니다.' });
+      payload.msg = '이메일 혹은 패스워드가 틀렸습니다.';
+
+      return res.status(200).json({ payload });
     }
     const { user_id, nickname } = userPasswordAndId[0];
     // 해쉬된 비밀번호와 유저가 입력한 비밀번호를 비교
@@ -121,12 +130,14 @@ const authUser = async (req, res) => {
       const token = jwt.sign({ user_id }, process.env.SECRET_KEY, {
         expiresIn: '60m',
       });
-      return res.status(200).json({ success: true, token, nickname });
+      payload.token = token;
+      payload.nickname = nickname;
+
+      return res.status(200).json({ payload });
     }
     //비밀번호가 틀려서 로그인 실패
-    return res
-      .status(200)
-      .json({ success: true, msg: '이메일 혹은 패스워드가 틀렸습니다' });
+    payload.msg = '이메일 혹은 패스워드가 틀렸습니다.';
+    return res.status(200).json({ payload });
   } catch (err) {
     logger.error(`해쉬된 비밀번호 가지고오는 과정에서 에러 : ${err}`);
     return res.status(400).json({
