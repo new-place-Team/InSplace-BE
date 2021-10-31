8f8121b5ad77ca367d4efe998cd9477bf2de3cf1
KimGiTae <rlxo6919@naver.com>
2021-10-31 17:25:15 +0900

    수정

--- routes/searching.js
+++ routes/searching.js
@@ -9,14 +9,7 @@ const {
 
 /* 조건 결과 페이지 조회 라우터 */
 
-<<<<<<< HEAD
-router.get('/condition', justCheckAuth, getResultPageOfCondition);
-router.get('/page/:pageNumber/condition', (req, res) => {
-  console.log('hi');
-});
-=======
 /* 조건 결과 상세 페이지 조회(실내외 구분) */
 router.get('/page/:number/condition', getDetailPageOfInOutDoors);
 
->>>>>>> 31f16955e804f6468e0cd31c491bafa0e4bbea45
 module.exports = router;
