4652d0cd67d260bd869fba8f4af8c9b4b55548a6
KimGiTae <rlxo6919@naver.com>
2021-11-01 12:44:13 +0900

    올림

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,16 +1,8 @@
-const Joi = require('Joi');
+const Joi = require('joi');
 
 const schemas = Joi.object({
   userId: Joi.number().min(1).required(),
   postId: Joi.number().min(1).required(),
 });
 
-const registSchemas = Joi.object({
-  email: Joi.number().min(1).required(),
-  nickname: Joi.number().min(1).required(),
-  password: Joi.number().min(1).required(),
-  male_yn: Joi.number().min(1).required(),
-  mbti_id: Joi.number().min(1).required(),
-});
-
-module.exports = { schemas, registSchemas };
+module.exports = schemas;
--- middlewares/validationUser.js
+++ middlewares/validationUser.js
@@ -1,37 +1,44 @@
 const Joi = require('joi');
 
 const Schema = Joi.object({
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ['com', 'net'] },
-  }),
-  nickname: Joi.string(),
-  password: Joi.string().min(4),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    })
+    .required(),
+  nickname: Joi.string().required(),
+  password: Joi.string().min(4).required(),
+  male_yn: Joi.number().integer().required(),
+  mbti_id: Joi.number().integer().required(),
 });
 
 const validationUser = async (req, res, next) => {
-  const { email, nickname, password } = req.body;
-
+  const { email, nickname, password, male_yn, mbti_id } = req.body;
+  console.log(male_yn, mbti_id);
   try {
     await Schema.validateAsync({
       email,
       nickname,
       password,
+      male_yn,
+      mbti_id,
     });
-    req.user = { email, nickname, password };
+    req.user = { email, nickname, password, male_yn, mbti_id };
     next();
   } catch (error) {
     const { message } = error;
+    console.log(message);
     let payload = {
       success: false,
     };
     if (message == '"email" must be a valid email') {
       payload.errMsg = '이메일 형식으로 작성해주세요';
-      res.status(200).json({ payload });
+      res.status(400).json({ payload });
     }
     if (message == '"password" length must be at least 4 characters long') {
       payload.errMsg = '비밀번호는 최소 4자 이상으로 작성해 주세요';
-      res.status(200).json({
+      res.status(400).json({
         payload,
       });
     }
