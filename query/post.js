7c97d90ae2ffb7e32b39e895cc4eaa8c4b090957
KimGiTae <rlxo6919@naver.com>
2021-11-01 17:38:31 +0900

    detail page 수정

--- query/post.js
+++ query/post.js
@@ -3,10 +3,17 @@ const addVisited = (userID, postID) => {
     (user_id, post_id)
     VALUES("${userID}","${postID}")`;
 };
-const findDetailPage = (postID) => {
+const findDetailPosts = (postID) => {
   return `SELECT title, post_images, contact_number, post_loc_x, post_loc_y, description, address, address_short, post_desc, like_cnt FROM Posts 
     INNER JOIN Categories 
     ON Posts.category_id = Categories.category_id  
     WHERE post_id = "${postID}"`;
 };
-module.exports = { addVisited, findDetailPage };
+
+const findDetailReviews = (postID) => {
+  return `SELECT user_Id AS userID, review_images AS reviewImages, review_desc AS reviewDesc, created_at AS createdAt FROM Reviews
+  WHERE post_id = "${postID}" AND delete_yn = "0"
+  ORDER BY created_at DESC 
+  LIMIT 8`;
+};
+module.exports = { addVisited, findDetailPosts, findDetailReviews };
