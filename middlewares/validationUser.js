2a8b3cdaed3bb330900480e3ab01e3f0b9a7a108
KimGiTae <rlxo6919@naver.com>
2021-11-01 14:44:02 +0900

    [20211101]메인페이지 로그인 오류 수정

--- middlewares/validationUser.js
+++ middlewares/validationUser.js
@@ -1,5 +1,5 @@
 const Joi = require('joi');
-
+const { customizedError } = require('../controllers/error');
 const Schema = Joi.object({
   email: Joi.string()
     .email({
@@ -15,7 +15,6 @@ const Schema = Joi.object({
 
 const validationUser = async (req, res, next) => {
   const { email, nickname, password, male_yn, mbti_id } = req.body;
-  console.log(male_yn, mbti_id);
   try {
     await Schema.validateAsync({
       email,
@@ -25,23 +24,10 @@ const validationUser = async (req, res, next) => {
       mbti_id,
     });
     req.user = { email, nickname, password, male_yn, mbti_id };
-    next();
+    return next();
   } catch (error) {
     const { message } = error;
-    console.log(message);
-    let payload = {
-      success: false,
-    };
-    if (message == '"email" must be a valid email') {
-      payload.errMsg = '이메일 형식으로 작성해주세요';
-      res.status(400).json({ payload });
-    }
-    if (message == '"password" length must be at least 4 characters long') {
-      payload.errMsg = '비밀번호는 최소 4자 이상으로 작성해 주세요';
-      res.status(400).json({
-        payload,
-      });
-    }
+    return next(customizedError(message, 400));
   }
 };
 
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const validationUser = require('../middlewares/validationUser');
+
 const { registUser, authUser, checkUser } = require('../controllers/user');
 /* GET users listing. */
 router.post('/register', validationUser, registUser);
-router.post('/auth', validationUser, authUser);
+router.post('/auth', authUser);
 router.post('/check/auth', checkUser);
 
 module.exports = router;
