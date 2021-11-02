f0ac162a5612b4198aed7d52787ecbe9e14487ab
fnrkp089 <fnrkp089@gmail.com>
2021-11-02 19:59:06 +0900

    [211102] 쿼리 1차수정

--- controllers/index.js
+++ controllers/index.js
@@ -9,25 +9,33 @@ const customizedError = require('./error');
 
 const searchMain = async (req, res, next) => {
   let weatherResult;
+  let user = 0;
   const connection = await pool.getConnection(async (conn) => conn);
+  
+  const adjImg = (result) => {
+    let resultImg = result[0];
+    for (let i = 0; i < resultImg.length; i++) {
+      resultImg[i].post_images = result[0][i].post_images
+        .split('&&')
+        .slice(1)[0];
+    }
+    return resultImg;
+  };
+
+  if(req.user){
+    user = req.user
+  }
+
   try {
-    const adjImg = (result) => {
-      let resultImg = result[0];
-      for (let i = 0; i < resultImg.length; i++) {
-        resultImg[i].post_images = result[0][i].post_images
-          .split('&&')
-          .slice(1)[0];
-      }
-      return resultImg;
-    };
+    
     weatherResult = await connection.query(weatherQuery);
     const weatherInfo = weatherResult[0]; //날씨 조회
     const weatherCondition = weatherInfo[0].weather_status; //날씨 상태 ID
     const weatherTemp = weatherInfo[0].weather_temp; //현재 온도
     const weatherDiff = weatherInfo[0].temp_diff; // 어제와 오늘 온도의 차이
-    const result = await connection.query(searchMainQuery(weatherCondition)); //날씨
+    const result = await connection.query(searchMainQuery(weatherCondition, user)); //날씨
     const likeResult = await connection.query(likeQuery); //좋아요
-    const mdResult = await connection.query(mdQuery); // 관리자 추천
+    const mdResult = await connection.query(mdQuery, user); // 관리자 추천
     const adjResult = adjImg(result);
     const adjLike = adjImg(likeResult);
     const adjMd = adjImg(mdResult);
