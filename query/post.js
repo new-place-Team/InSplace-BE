0d4fb68b7dbf2cc96379df80cc36a49bebabc5cf
KimGiTae <rlxo6919@naver.com>
2021-11-02 21:24:26 +0900

    [20211102]디테일 좋아요 여부 수정

--- query/post.js
+++ query/post.js
@@ -6,25 +6,27 @@ const addVisited = (userID, postID) => {
 const findDetailPosts = (postID, userID) => {
   return `
 
-  SELECT post_images AS postImages, contact_number AS contactNumber,
-  post_loc_x, post_loc_y, description, address, 
-  address_short AS addressShort, post_desc AS postDesc, favorite_cnt AS favoriteCnt,
-  CASE WHEN Favorites.user_id = "${userID}"
-  THEN 1
-  ELSE 0
-  END AS favoriteState ,
-  CASE WHEN VisitedPosts.user_id = "${userID}"
-  THEN 1
-  ELSE 0
-  END AS visitedStatus
-  FROM Posts 
-  INNER JOIN Categories 
-  ON Posts.category_id = Categories.category_id   
-  LEFT JOIN Favorites
-  ON Posts.post_id = Favorites.post_id
-  LEFT JOIN VisitedPosts
-  ON Posts.post_id = VisitedPosts.post_id
-  WHERE Posts.post_id = "${postID}"
+  SELECT 
+  post_images AS postImages, contact_number AS contactNumber,
+   post_loc_x, post_loc_y, description, address, 
+   address_short AS addressShort, post_desc AS postDesc, favorite_cnt AS favoriteCnt,
+   CASE WHEN Favorites.user_id = "${userID}"
+   THEN 1
+   ELSE 0
+   END AS favoriteState ,
+   CASE WHEN VisitedPosts.user_id = "${userID}"
+   THEN 1
+   ELSE 0
+   END AS visitedStatus
+   FROM Posts 
+   INNER JOIN Categories 
+   ON Posts.category_id = Categories.category_id   
+   LEFT JOIN Favorites
+   ON Posts.post_id = Favorites.post_id AND Favorites.user_id="${userID}"
+   LEFT JOIN VisitedPosts
+   ON Posts.post_id = VisitedPosts.post_id AND VisitedPosts.user_id="${userID}"
+   WHERE Posts.post_id = "${postID}"
+
 `;
 };
 
@@ -34,18 +36,19 @@ const checkVisitedUser = (userID, postID) => {
 
 const findDetailReviews = (postID, userID) => {
   return `
-
-  SELECT Reviews.user_Id AS userID, review_images AS reviewImages, review_desc AS reviewDesc, created_at AS createdAt,
+  SELECT 
+  Reviews.user_Id AS userID, review_images AS reviewImages, 
+  review_desc AS reviewDesc, created_at AS createdAt,
   CASE 
-  WHEN ReviewLikes.user_id = 4
+  WHEN ReviewLikes.user_id = "${userID}"
   THEN 1
   ELSE 0
   END AS likeStatus
   FROM Reviews
   LEFT JOIN ReviewLikes
   ON
-  Reviews.review_id = ReviewLikes.review_id
-  WHERE post_id = "${postID}" AND delete_yn = "0"
+  Reviews.review_id = ReviewLikes.review_id AND ReviewLikes.user_id = "${userID}"
+  WHERE post_id ="${postID}" AND delete_yn = "0"
   ORDER BY created_at DESC 
   LIMIT 8
 `;
