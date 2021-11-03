07537e78311f246cb201e8a8b185225918e9a521
fnrkp089 <fnrkp089@gmail.com>
2021-11-03 13:48:06 +0900

    [211103] 메인쿼리 유저수정

--- query/main.js
+++ query/main.js
@@ -48,17 +48,19 @@ ORDER BY favorite_cnt DESC limit 14
 `;
 }
 
-const mdQuery = `
-SELECT DISTINCT Posts.post_id, Posts.title, Posts.address, Posts.address_short, Posts.contact_number, Posts.category_id, Posts.post_images, Posts.post_desc, Posts.post_loc_x, Posts.post_loc_y, Posts.favorite_cnt, Posts.weather_id, Posts.inside_yn, Posts.gender_id, Posts.member_id, 
-	CASE 
-		WHEN Favorites.user_Id = ? THEN true 
-		ELSE false 
-    END as 'favoriteState'
-FROM Posts 
-LEFT JOIN Favorites
-ON Posts.post_id = Favorites.post_id
-WHERE Posts.md_pick = 1
-`; // 해당쿼리는 수정 예정
+const mdQuery = (user) => { 
+  return `
+  SELECT DISTINCT Posts.post_id, Posts.title, Posts.address, Posts.address_short, Posts.contact_number, Posts.category_id, Posts.post_images, Posts.post_desc, Posts.post_loc_x, Posts.post_loc_y, Posts.favorite_cnt, Posts.weather_id, Posts.inside_yn, Posts.gender_id, Posts.member_id, 
+    CASE 
+      WHEN Favorites.user_Id = ${user} THEN true 
+      ELSE false 
+      END as 'favoriteState'
+  FROM Posts 
+  LEFT JOIN Favorites
+  ON Posts.post_id = Favorites.post_id
+  WHERE Posts.md_pick = 1
+`; 
+}// 해당쿼리는 수정 예정
 
 const weatherQuery = `
 SELECT *
