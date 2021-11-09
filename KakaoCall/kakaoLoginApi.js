bd963a63e63aa0d7c56bdff1b7bf9fd55ed79467
KimGiTae <rlxo6919@naver.com>
2021-11-09 12:05:57 +0900

    Kakao test

--- KakaoCall/kakaoLoginApi.js
+++ KakaoCall/kakaoLoginApi.js
@@ -12,7 +12,9 @@ const getKakaoToken = async (code) => {
       },
       data: qs.stringify({
         grant_type: 'authorization_code',
-        redirect_uri: 'https://insplace-dev.web.app/users/kakao/auth',
+        redirect_uri: encodeURI(
+          'https://insplace-dev.web.app/users/kakao/auth'
+        ),
         client_id: process.env.KAKAO_REST_KEY, //env파일로
         code,
       }),
