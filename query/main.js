ae31770a2a960bf5471d180bf578cccde0f7998e
fnrkp089 <fnrkp089@gmail.com>
2021-11-11 18:38:21 +0900

    [211111] 메인페이지 쿼리(8개), 날씨 중복카드 삭제

--- query/main.js
+++ query/main.js
@@ -1,46 +1,49 @@
 const searchMainQuery = (weatherCondition, user) => {
   return `
-SELECT DISTINCT
-  Posts.post_id AS postId, 
-  Posts.title, 
-  Posts.address, 
-  Posts.address_short AS addressShort, 
-  Posts.contact_number AS contactNumber, 
-  Posts.category_id AS category, 
-  Posts.post_images AS postImage,
-  Posts.post_desc AS PostDesc,
-  Posts.post_loc_x AS postLocationX, 
-  Posts.post_loc_y AS postLocationY, 
-  Posts.favorite_cnt AS favoriteCnt, 
-  Posts.weather_id AS weatherId, 
-  Posts.inside_yn AS insideYN, 
-  Posts.gender_id AS genderId, 
-  Posts.member_id AS MemberId, 
+  SELECT DISTINCT
+  a.post_id AS postId, 
+  a.title, 
+  a.address, 
+  a.address_short AS addressShort, 
+  a.contact_number AS contactNumber, 
+  a.category_id AS category, 
+  a.post_images AS postImage,
+  a.post_desc AS PostDesc,
+  a.post_loc_x AS postLocationX, 
+  a.post_loc_y AS postLocationY, 
+  a.favorite_cnt AS favoriteCnt, 
+  a.weather_id AS weatherId, 
+  a.inside_yn AS insideYN, 
+  a.gender_id AS genderId, 
+  a.member_id AS MemberId, 
 	CASE 
-		WHEN Favorites.user_Id = ${user} THEN true 
+		WHEN b.user_Id = ${user} THEN true 
 		ELSE false 
     END as 'favoriteState'
-FROM Posts 
-LEFT JOIN Favorites
-ON Posts.post_id = Favorites.post_id
+FROM Posts a
+LEFT JOIN
+(
+	SELECT post_id, user_id
+    FROM Favorites
+    where user_id = ${user}
+) b ON a.post_id = b.post_id
 LEFT JOIN VisitedPosts
-ON Posts.post_id = VisitedPosts.post_id
+ON a.post_id = VisitedPosts.post_id
 WHERE weather_id IN(${weatherCondition}, 7)
-  AND Posts.post_id NOT IN(
+  AND a.post_id NOT IN(
     SELECT post_id
     FROM(
       SELECT post_id
       FROM Posts
-      ORDER BY favorite_cnt DESC limit 14
+      ORDER BY favorite_cnt DESC limit 9
     ) as tempPosts 
   )
-  AND Posts.post_id NOT IN (
+  AND a.post_id NOT IN (
     SELECT post_id
       FROM VisitedPosts
       WHERE VisitedPosts.user_id = ${user}
     )
-  ORDER BY rand() limit 14
-
+  ORDER BY rand() limit 9
   `;
 };
 
@@ -78,7 +81,7 @@ WHERE a.post_id NOT IN (
     FROM VisitedPosts
     WHERE VisitedPosts.user_id = ${user}
     )
-ORDER BY favorite_cnt DESC limit 14
+ORDER BY favorite_cnt DESC limit 9
 `;
 }
 
