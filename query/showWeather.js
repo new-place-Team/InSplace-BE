3030f56614063deb6ca5806ff8a5fcf8bf7f47c1
fnrkp089 <fnrkp089@gmail.com>
2021-11-16 17:49:25 +0900

    [211116] 습도, 미세먼지, 초미세먼지 프론트전달

--- query/showWeather.js
+++ query/showWeather.js
@@ -3,7 +3,10 @@ SELECT
   weather_status AS status,
   weather_temp AS temperature,
   temp_diff AS diff,
-  weather_status_fe AS frontWeather
+  weather_status_fe AS frontWeather,
+  humidity,
+  pm10,
+  pm25
 FROM CurrentWeather
 WHERE cur_weather_id = 0
 `;
