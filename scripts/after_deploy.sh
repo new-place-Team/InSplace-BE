5d375adbd8a442cb1b788433889a3145312dac93
KimGiTae <rlxo6919@naver.com>
2021-11-16 13:39:34 +0900

    docker 변경

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -5,5 +5,5 @@ echo 'script start'
 cd /home/ubuntu/insplace
 sudo docker stop $(sudo docker ps -aq)
 sudo docker build -t insplace .
-sudo docker run -p 5000:5000 insplace
-sudo docker run -p 5001:5000 insplace
+sudo docker run -p 5000:5000 -d insplace
+sudo docker run -p 5001:5000 -d insplace
