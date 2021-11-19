c6b58f67d7b15bd2892e578748a8548683e4b3e6
KimGiTae <rlxo6919@naver.com>
2021-11-20 02:17:39 +0900

    [211120] after_deploy file 수정

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -6,5 +6,3 @@ cd /home/ubuntu/insplace
 sudo docker stop $(sudo docker ps -aq)
 sudo docker build -t insplace .
 sudo docker run -p 5000:5000 -d insplace
-sudo docker run -p 5001:5000 -d insplace
-sudo docker run -p 5002:5000 -d insplace
