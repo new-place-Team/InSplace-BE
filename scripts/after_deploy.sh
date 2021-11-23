21f4504fbd528ca62651f14b3489c48ef03dba9a
KimGiTae <rlxo6919@naver.com>
2021-11-23 17:29:52 +0900

    [20211123] pro server ci/cd test10

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,6 +2,6 @@
 
 echo 'script start'
 
-cd /home/ubuntu/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/bin
+cd /home/ubuntu
 
  kubectl get pods
