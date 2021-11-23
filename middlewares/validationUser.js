c354fa11aea9d7eb71249c16730cbb30ef6b4a52
KimGiTae <rlxo6919@naver.com>
2021-11-24 05:43:26 +0900

    [20211124] 회원가입 에러핸들러

--- middlewares/validationUser.js
+++ middlewares/validationUser.js
@@ -16,8 +16,9 @@ const Schema = Joi.object({
 });
 
 const validationUser = async (req, res, next) => {
+  const lang = req.headers['language'];
   const { email, nickname, password, maleYN, mbtiId } = req.body;
-
+  let errMsg = '';
   try {
     await Schema.validateAsync({
       email,
@@ -30,6 +31,29 @@ const validationUser = async (req, res, next) => {
     return next();
   } catch (error) {
     const { message } = error;
+    if (message.includes('fails to match')) {
+      errMsg =
+        lang === 'kr' || lang === undefined
+          ? '비밀번호는 영어와 숫자의 조합으로 8자 이상 16자 이하로 작성해주세요.'
+          : 'Please write the password in English and Numbers, not less than 8 characters and not more than 16 characters.';
+
+      return next(customizedError(errMsg, 400));
+    }
+    if (message.includes('email')) {
+      errMsg =
+        lang === 'kr' || lang === undefined
+          ? '이메일 양식으로 입력해 주세요.'
+          : 'Please fill in the email form.';
+      return next(customizedError(errMsg, 400));
+    }
+    if (message.includes('nickname')) {
+      errMsg =
+        lang === 'kr' || lang === undefined
+          ? '닉네임은 2자 이상 12자 이하로 작성해 주세요.'
+          : 'Please write your nickname with at least 2 characters and no more than 12 characters.';
+      return next(customizedError(errMsg, 400));
+    }
+
     return next(customizedError(message, 400));
   }
 };
